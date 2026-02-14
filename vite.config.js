0f374862b5db99ecd79151785d250e7f7496e596
melcikoo <34906228+melcikoo@users.noreply.github.com>
2026-02-14 13:46:03 +0900

    test

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://github.com/melcikoo/word-journey/',  // GitHub Pages repo name - change this to match your repo
+  // 전체 URL 대신, 배포되는 하위 경로(레포지토리 명)만 적어줍니다.
+  base: '/word-journey/', 
 })
